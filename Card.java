842d6ef8497b59bac14f34b4d6a3f6bc5c2f94dd
Ami1423 <63359009+Ami1423@users.noreply.github.com>
2020-06-17 15:34:02 -0400

    Add a new column to the card file.
    Add column to the existing file.

---
+++ Card.java
@@ -0,0 +1,12 @@
+package com.example.CreditCardMicroservice;
+
+public class Card {
+	
+	public int cardNumber;
+	public String name;
+	public double cvv;
+	public double availableCredit;
+	public String status;
+		
+
+}
